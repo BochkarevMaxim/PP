441d08cbd4d6fcda12641e6f042d59d668d7f65c
Bochkarev Maxim <qontrol1488@gmail.com>
2017-01-16 13:38:31 +0300

    Added multi-threaded inverse matrix calculation

--- PP Lab 1/src/Main.java
+++ PP Lab 1/src/Main.java
@@ -1,27 +1,46 @@
 import Matrix.Matrix;
+import Matrix.MatrixParallelMathematics;
 import Matrix.NoSquareException;
 
 import static Matrix.MatrixMathematics.getInversedMatrix;
+import static Matrix.MatrixParallelMathematics.parallelGetInversedMatrix;
 
 public class Main {
 
     public static void main(String[] args){
 
+        final int threadsNum = 4;
         Matrix mx = new Matrix (10, 10);
 
-        // Single-threaded getInversedMatrix matrix calculation
-
-        //System.out.println(Arrays.deepToString(mx.getValues()));
+        // SINGLE-THREADED inverse matrix calculation
         long startTime = System.currentTimeMillis();
         try{
             Matrix singleThreadMatrix = getInversedMatrix(mx);
-           // System.out.println(Arrays.deepToString(getInversedMatrix(mx).getValues()));
         } catch (NoSquareException exc){
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
         long endTime   = System.currentTimeMillis();
         long iterativeTotalTime = endTime - startTime;
-        System.out.println("Iterative time " + iterativeTotalTime);
+        System.out.println("SINGLE-THREADED inverse matrix calculation time: " + iterativeTotalTime + " ms");
+
+        MatrixParallelMathematics matrixParallelMathematics = new MatrixParallelMathematics(threadsNum);
+
+        // MULTI-THREADED inverse matrix calculation
+        long asyncStartTime = System.currentTimeMillis();
+        try{
+            Matrix asyncThreadMatrix = parallelGetInversedMatrix(mx);
+        } catch (NoSquareException exc){
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long asyncEndTime   = System.currentTimeMillis();
+        long asyncTotalTime = asyncEndTime - asyncStartTime;
+        System.out.println("MULTI-THREADED inverse matrix calculation time: " + asyncTotalTime + " ms");
 
     }
+
+
 }
