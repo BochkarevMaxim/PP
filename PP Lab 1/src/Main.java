1a05ba7ce3d8ddb144598e3c12525790335f9631
Bochkarev Maxim <qontrol1488@gmail.com>
2017-01-16 03:15:19 +0300

    Added timer for singlethreded calculation

--- PP Lab 1/src/Main.java
+++ PP Lab 1/src/Main.java
@@ -1,20 +1,27 @@
 import Matrix.Matrix;
 import Matrix.NoSquareException;
 
-import java.util.Arrays;
-
-import static Matrix.MatrixMathematics.inverse;
+import static Matrix.MatrixMathematics.getInversedMatrix;
 
 public class Main {
 
     public static void main(String[] args){
-        Matrix mx = new Matrix (3, 3);
-        System.out.println(Arrays.deepToString(mx.getValues()));
+
+        Matrix mx = new Matrix (10, 10);
+
+        // Single-threaded getInversedMatrix matrix calculation
+
+        //System.out.println(Arrays.deepToString(mx.getValues()));
+        long startTime = System.currentTimeMillis();
         try{
-            System.out.println(Arrays.deepToString(inverse(mx).getValues()));
+            Matrix singleThreadMatrix = getInversedMatrix(mx);
+           // System.out.println(Arrays.deepToString(getInversedMatrix(mx).getValues()));
         } catch (NoSquareException exc){
 
         }
+        long endTime   = System.currentTimeMillis();
+        long iterativeTotalTime = endTime - startTime;
+        System.out.println("Iterative time " + iterativeTotalTime);
 
     }
 }
